01313b4ccd2252fc0592935142b520a5e18ad0a5
PatrykDzi <91917759+PatrykDzi@users.noreply.github.com>
2022-12-02 12:57:50 +0100

    Laboratorium 6

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const config = {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    databaseUrl: "mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 };
 
 module.exports = {
-    config: config
+    config: config,
 }
